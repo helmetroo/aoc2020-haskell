8d6d96cff7f778d17188688b4dc0ce5e4098be9f
Samuel Sweet <samueljsweet@gmail.com>
2021-01-10 19:32:55 -0700

    Solution for P11 p2 (augment p1)

--- src/P11/SeatMap.hs
+++ src/P11/SeatMap.hs
@@ -2,7 +2,9 @@ module P11.SeatMap(
   SeatMap(..),
   toSeatMap,
   applyOccupancyRules,
-  countOccupiedSeats
+  countOccupiedSeats,
+  usingFirstRules,
+  usingSecondRules
   ) where
 
 import Control.Applicative(
@@ -24,6 +26,13 @@ import Data.Array(
 type Indices = (Int, Int)
 data Tile = Floor | EmptySeat | OccupiedSeat
   deriving Eq
+instance Show Tile where
+  show tile =
+    case tile of
+      Floor -> "."
+      EmptySeat -> "L"
+      OccupiedSeat -> "#"
+
 type SeatMapArray = Array Indices Tile
 type Association = (Indices, Tile)
 type AssociationList = [Association]
@@ -34,12 +43,22 @@ data SeatMap = SeatMap {
   seats :: [Indices]
   }
 
-instance Show Tile where
-  show tile =
-    case tile of
-      Floor -> "."
-      EmptySeat -> "L"
-      OccupiedSeat -> "#"
+data Direction = N | NE | E | SE | S | SW | W | NW
+  deriving Enum
+
+toIncrement :: Direction -> Indices
+toIncrement dir =
+  case dir of
+    N  -> (-1, 0)
+    NE -> (-1, 1)
+    E  -> (0, 1)
+    SE -> (1, 1)
+    S  -> (1, 0)
+    SW -> (1, -1)
+    W  -> (0, -1)
+    NW -> (-1, -1)
+
+directions = [N .. NW]
 
 toTile :: Char -> Tile
 toTile c =
@@ -56,23 +75,50 @@ isEmptySeat = (== EmptySeat)
 isSeat :: Tile -> Bool
 isSeat = liftA2 (||) isOccupiedSeat isEmptySeat
 
-adjacentIndices :: Indices -> Indices -> [Indices]
-adjacentIndices (rows, cols) (row, col) =
+type IndicesSearch = SeatMapArray -> Indices -> Indices -> [Indices]
+
+adjacentIndices :: IndicesSearch
+adjacentIndices _ (rows, cols) (row, col) =
   [(adjRow, adjCol) |
-   adjRow <- [row - 1 .. row + 1],
-   adjCol <- [col - 1 .. col + 1],
-   adjRow >= 0 && adjCol >= 0 &&
-   adjRow < rows && adjCol < cols &&
-   (adjRow, adjCol) /= (row, col)]
+    adjRow <- [row - 1 .. row + 1],
+    adjCol <- [col - 1 .. col + 1],
+    withinBounds (rows, cols) (adjRow, adjCol) &&
+    (adjRow, adjCol) /= (row, col)]
+
+withinBounds :: Indices -> Indices -> Bool
+withinBounds (rows, cols) (row, col) =
+    row >= 0 && col >= 0 &&
+    row < rows && col < cols
+
+visibleSeatIndices :: IndicesSearch
+visibleSeatIndices mapLayout bounds pos =
+  foldMap (toList . findIndicesOfSeat bounds pos mapLayout) directions
+
+findIndicesOfSeat :: Indices -> Indices -> SeatMapArray -> Direction -> Maybe Indices
+findIndicesOfSeat bounds pos mapLayout dir =
+  findIndicesOfSeatFrom bounds pos mapLayout (toIncrement dir)
+
+findIndicesOfSeatFrom :: Indices -> Indices -> SeatMapArray -> Indices -> Maybe Indices
+findIndicesOfSeatFrom bounds pos mapLayout increment =
+  let newPos = addIndices increment pos
+      inBounds = withinBounds bounds newPos
+  in if not inBounds
+     then Nothing
+     else if isSeat $ mapLayout ! newPos
+          then Just newPos
+          else findIndicesOfSeatFrom bounds newPos mapLayout increment
+
+addIndices :: Indices -> Indices -> Indices
+addIndices a b = (fst a + fst b, snd a + snd b)
 
 countOccupiedSeats :: SeatMapArray -> [Indices] -> Int
 countOccupiedSeats mapLayout =
   length . filter isOccupiedSeat . map (mapLayout !)
 
-countOccupiedSeatsFrom :: SeatMapArray -> Indices -> Indices -> Int
-countOccupiedSeatsFrom mapLayout mapBounds seatIndices =
-  let adjacentTiles = map (mapLayout !) $ adjacentIndices mapBounds seatIndices
-  in length $ filter isOccupiedSeat adjacentTiles
+countOccupiedSeatsFrom :: IndicesSearch -> SeatMapArray -> Indices -> Indices -> Int
+countOccupiedSeatsFrom findIndices mapLayout mapBounds seatIndices =
+  let tiles = map (mapLayout !) $ findIndices mapLayout mapBounds seatIndices
+  in length $ filter isOccupiedSeat tiles
 
 toSeatMap :: [String] -> SeatMap
 toSeatMap seatMapLines =
@@ -94,18 +140,23 @@ toAssociationListFrom (currentLine : restLines) bounds row assocList =
       assocListForRow = zip indices tiles
   in toAssociationListFrom restLines bounds (row + 1) (assocListForRow ++ assocList)
 
-applyOccupancyRules :: SeatMap -> SeatMap
-applyOccupancyRules currentMap =
+type ChangesApplier = SeatMapArray -> Indices -> Indices -> Maybe Association
+
+applyOccupancyRules :: ChangesApplier -> SeatMap -> SeatMap
+applyOccupancyRules applySeatChanges currentMap =
   let mapLayout = layout currentMap
       mapBounds = bounds currentMap
       seatIndices = seats currentMap
-      changes = foldMap (toList . changesForSeat mapLayout mapBounds) seatIndices
+      changes = foldMap (toList . applySeatChanges mapLayout mapBounds) seatIndices
   in currentMap { layout = mapLayout // changes }
 
-changesForSeat :: SeatMapArray -> Indices -> Indices -> Maybe Association
-changesForSeat mapLayout mapBounds seatIndices =
- let occupiedSeats = countOccupiedSeatsFrom mapLayout mapBounds seatIndices
+changesForSeat :: Int -> IndicesSearch -> ChangesApplier
+changesForSeat occupiedCount findIndices mapLayout mapBounds seatIndices =
+ let occupiedSeats = countOccupiedSeatsFrom findIndices mapLayout mapBounds seatIndices
      seat = mapLayout ! seatIndices
  in case seat of
    EmptySeat -> if occupiedSeats == 0 then Just (seatIndices, OccupiedSeat) else Nothing
-   OccupiedSeat -> if occupiedSeats >= 4 then Just (seatIndices, EmptySeat) else Nothing
+   OccupiedSeat -> if occupiedSeats >= occupiedCount then Just (seatIndices, EmptySeat) else Nothing
+
+usingFirstRules = changesForSeat 4 adjacentIndices
+usingSecondRules = changesForSeat 5 visibleSeatIndices
